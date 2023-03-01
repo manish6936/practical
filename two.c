8d7b3a7654e24a96d8c1d098ad18832809fc87fd
manish6936 <104090206+manish6936@users.noreply.github.com>
2023-03-01 12:28:28 +0545

    Update two.c

--- two.c
+++ two.c
@@ -1,41 +1,50 @@
-2.WAP to input elements of two (3x3)matricesand print sum of it in matrix format
 #include <stdio.h>
 
-int main() {
-    int matrix1[3][3], matrix2[3][3], sum_matrix[3][3];
+void add_matrices(int a[][3], int b[][3], int c[][3]);
+
+int main()
+{
+    int a[3][3], b[3][3], c[3][3];
     int i, j;
 
-    // Taking input of the first matrix
-    printf("Enter the elements of the first matrix:\n");
-    for(i=0; i<3; i++) {
-        for(j=0; j<3; j++) {
-            scanf("%d", &matrix1[i][j]);
+    // input first matrix
+    printf("Enter elements of first matrix:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            scanf("%d", &a[i][j]);
         }
     }
 
-    // Taking input of the second matrix
-    printf("Enter the elements of the second matrix:\n");
-    for(i=0; i<3; i++) {
-        for(j=0; j<3; j++) {
-            scanf("%d", &matrix2[i][j]);
+    // input second matrix
+    printf("Enter elements of second matrix:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            scanf("%d", &b[i][j]);
         }
     }
 
-    // Adding the matrices
-    for(i=0; i<3; i++) {
-        for(j=0; j<3; j++) {
-            sum_matrix[i][j] = matrix1[i][j] + matrix2[i][j];
-        }
-    }
+    // add matrices
+    add_matrices(a, b, c);
 
-    // Printing the sum matrix in matrix format
-    printf("The sum of the matrices is:\n");
-    for(i=0; i<3; i++) {
-        for(j=0; j<3; j++) {
-            printf("%d ", sum_matrix[i][j]);
+    // print result
+    printf("Resultant matrix:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            printf("%d ", c[i][j]);
         }
         printf("\n");
     }
 
     return 0;
+}
+
+void add_matrices(int a[][3], int b[][3], int c[][3])
+{
+    int i, j;
+
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            c[i][j] = a[i][j] + b[i][j];
+        }
+    }
 }
